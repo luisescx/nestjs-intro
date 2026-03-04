623993de5b4ea7e4959c0bc7914debae00185113
luisescx <luis.esculapio@live.com>
2026-03-04 14:42:37 -0300

    feat: add user module

---
+++ src/users/users.controller.ts
@@ -0,0 +1,38 @@
+import {
+  Body,
+  Controller,
+  DefaultValuePipe,
+  Get,
+  Param,
+  ParseIntPipe,
+  Patch,
+  Post,
+  Query,
+} from '@nestjs/common';
+import { CreateUserDto } from './dtos/create-user.dto';
+import { GetUserParamsDto } from './dtos/get-user-params.dto';
+import { PatchUserDto } from './dtos/patch-user.dto';
+
+@Controller('users')
+export class UsersController {
+  @Get('/{:id}')
+  getUsers(
+    @Param() getUserParamsDto: GetUserParamsDto,
+    @Query('limit', new DefaultValuePipe(10), ParseIntPipe) limit: number,
+    @Query('page', new DefaultValuePipe(1), ParseIntPipe) page: number,
+  ) {
+    console.log(getUserParamsDto, limit, page);
+    return 'request to users endpoint';
+  }
+
+  @Post()
+  createUsers(@Body() createUserDto: CreateUserDto) {
+    console.log(createUserDto);
+    return 'You sent a post request to users endpoint';
+  }
+
+  @Patch()
+  public patchUser(@Body() patchUserDto: PatchUserDto) {
+    return patchUserDto;
+  }
+}
