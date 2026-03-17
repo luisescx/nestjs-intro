32d2ccdfd5d09cdfd42c489bdc74a75fdfdbbc88
luisescx <luis.esculapio@live.com>
2026-03-17 15:30:12 -0300

    chore: add controllers mocks

--- src/posts/posts.controller.ts
+++ src/posts/posts.controller.ts
@@ -1,12 +1,33 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import { Body, Controller, Get, Param, Patch, Post } from '@nestjs/common';
 import { PostsService } from './providers/posts.service';
+import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
+import { CreatePostDto } from './dtos/create-post.dto';
+import { PatchPostDto } from './dtos/patch-post.dto';
 
 @Controller('posts')
+@ApiTags('Posts')
 export class PostsController {
   constructor(private readonly postService: PostsService) {}
 
   @Get('/{:id}')
   getPosts(@Param('userId') userId: number) {
     return this.postService.findAll(userId);
+  }
+
+  @ApiOperation({
+    summary: 'Blog post',
+  })
+  @ApiResponse({
+    status: 201,
+    description: 'You get a 201 response if your post is created successfully',
+  })
+  @Post()
+  public createPost(@Body() createPostDto: CreatePostDto) {
+    console.log(createPostDto);
+  }
+
+  @Patch()
+  updatePost(@Body() patchPostDto: PatchPostDto) {
+    console.log(patchPostDto);
   }
 }
