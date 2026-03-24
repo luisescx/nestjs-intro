7fa2df5b557716b540ce50720abf38ff0d43a9d4
luisescx <luis.esculapio@live.com>
2026-03-24 15:04:49 -0300

    feat: typeorm config

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,34 @@ import { UsersModule } from './users/users.module';
 import { PostsModule } from './posts/posts.module';
 import { AuthController } from './auth/auth.controller';
 import { AuthModule } from './auth/auth.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
-  imports: [UsersModule, PostsModule, AuthModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [],
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => {
+        return {
+          type: 'postgres',
+          host: config.get<string>('DB_HOST'),
+          port: 5432,
+          username: config.get<string>('DB_USERNAME'),
+          password: config.get<string>('DB_PASSWORD'),
+          database: config.get<string>('DB_NAME'),
+          synchronize: true,
+          entities: [],
+        };
+      },
+    }),
+    UsersModule,
+    PostsModule,
+    AuthModule,
+  ],
   controllers: [AppController, AuthController],
   providers: [AppService],
 })
